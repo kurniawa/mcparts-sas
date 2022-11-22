bab55c2dd611a63fe66e272da58fa9e492a366d8
Adi Kurniawan <kurniawan_01@ymail.com>
2022-11-22 08:08:24 +0700

    first commit: instalasi tailwind+livewire

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  @livewireStyles()
+</head>
+<body class="font-sans">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  @livewireScripts()
+</body>
+</html>
