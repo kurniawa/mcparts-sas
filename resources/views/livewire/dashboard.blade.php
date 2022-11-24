2a71190d2738e5e35ccc05c381f6d303f3ce2ac8
Adi Kurniawan <kurniawan_01@ymail.com>
2022-11-25 01:15:36 +0700

    favicon.ico + benerin tailwindcss classes

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -42,9 +42,9 @@
                 <h3 class="text-slate-600">Nama</h3>
                 <div class="flex items-center">
                     <div class="w-full">
-                        <input type="text" name="nama" id="nama" class="input" wire:model="user.nama">
+                        <input type="text" name="nama" id="nama" class="border border-slate-400 text-slate-700 shadow rounded w-full px-3 py-2 block placeholder:text-slate-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue-500 invalid:text-pink-700 invalid:focus:ring-pink-700;" wire:model="user.nama">
                         @error('user.nama')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                        <div class="w-full px-2 py-1 rounded text-pink-600">{{ $message }}</div>
                         @enderror
                     </div>
                     <svg x-on:click="open=!open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-slate-500 ml-1 hover:cursor-pointer">
@@ -53,13 +53,16 @@
                 </div>
                 <div class="mt-2">
                     @if (session()->has('nama_updated'))
-                    <div class="alert-success">
+                    <div class="font-semibold w-full px-3 py-2 rounded bg-emerald-200 text-emerald-600 opacity-70">
                         {{ session('nama_updated') }}
                     </div>
                     @endif
                 </div>
                 <div x-show="open" x-transition x-cloak class="text-right mt-5">
-                    <button class="btn-indigo-sm rounded">Confirm</button>
+                    <button
+                    class="rounded px-3 py-2 text-xs font-semibold bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:ring focus:ring-blue-300">
+                        Confirm
+                    </button>
                 </div>
 
             </form>
@@ -68,20 +71,23 @@
             <form wire:submit.prevent="updateUsername" x-data="{open:false}" class="mt-5 sm:w-full p-5 border rounded border-slate-300 bg-white shadow-xl max-w-sm">
                 <h3 class="text-slate-600">Username</h3>
                 <div class="flex items-center">
-                    <input type="text" name="username" id="username" class="input" wire:model="user.username">
+                    <input type="text" name="username" id="username" class="border border-slate-400 text-slate-700 shadow rounded w-full px-3 py-2 block placeholder:text-slate-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue-500 invalid:text-pink-700 invalid:focus:ring-pink-700;" wire:model="user.username">
                     <svg x-on:click="open=!open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-slate-500 ml-1 hover:cursor-pointer">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
                 </div>
                 <div class="mt-2">
                     @if (session()->has('username_updated'))
-                    <div class="alert-success">
+                    <div class="font-semibold w-full px-3 py-2 rounded bg-emerald-200 text-emerald-600 opacity-70">
                         {{ session('username_updated') }}
                     </div>
                     @endif
                 </div>
                 <div x-show="open" x-transition x-cloak class="text-right mt-5">
-                    <button class="btn-indigo-sm rounded">Confirm</button>
+                    <button
+                    class="rounded px-3 py-2 text-xs font-semibold bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:ring focus:ring-blue-300">
+                        Confirm
+                    </button>
                 </div>
             </form>
 
@@ -89,36 +95,43 @@
             <form wire:submit.prevent="updatePassword" x-data="{open:false}" class="mt-5 sm:w-full p-5 border rounded border-slate-300 bg-white shadow-xl max-w-sm">
                 <h3 class="text-slate-600">Password</h3>
                 <label for="current_password" class="text-slate-600">Current Password :</label>
-                <input wire:model="current_password" type="password" name="current_password" id="current_password" class="input" placeholder="Current Password">
+                <input wire:model="current_password" type="password" name="current_password" id="current_password" placeholder="Current Password"
+                class="border border-slate-400 text-slate-700 shadow rounded w-full px-3 py-2 block placeholder:text-slate-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue-500 invalid:text-pink-700 invalid:focus:ring-pink-700;"
+                >
                 @error('current_password')
-                <div class="invalid-feedback">{{ $message }}</div>
+                <div class="w-full px-2 py-1 rounded text-pink-600">{{ $message }}</div>
                 @enderror
                 <label for="new_password" class="text-slate-600 block mt-2">New Password :</label>
-                <input wire:model="new_password" type="password" name="new_password" id="new_password" class="input" placeholder="New Password">
+                <input wire:model="new_password" type="password" name="new_password" id="new_password" placeholder="New Password"
+                class="border border-slate-400 text-slate-700 shadow rounded w-full px-3 py-2 block placeholder:text-slate-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue-500 invalid:text-pink-700 invalid:focus:ring-pink-700;">
                 @error('new_password')
-                <div class="invalid-feedback">{{ $message }}</div>
+                <div class="w-full px-2 py-1 rounded text-pink-600">{{ $message }}</div>
                 @enderror
                 <label for="confirm_new_password" class="text-slate-600 block mt-2">Confirm New Password :</label>
-                <input wire:model="confirm_new_password" type="password" name="confirm_new_password" id="confirm_new_password" class="input" placeholder="Confirm New Password">
+                <input wire:model="confirm_new_password" type="password" name="confirm_new_password" id="confirm_new_password" placeholder="Confirm New Password"
+                class="border border-slate-400 text-slate-700 shadow rounded w-full px-3 py-2 block placeholder:text-slate-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue-500 invalid:text-pink-700 invalid:focus:ring-pink-700;">
                 @error('confirm_new_password')
-                <div class="invalid-feedback">{{ $message }}</div>
+                <div class="w-full px-2 py-1 rounded text-pink-600">{{ $message }}</div>
                 @enderror
 
                 <div class="mt-2">
                     @if (session()->has('password_updated'))
-                    <div class="alert-success">
+                    <div class="font-semibold w-full px-3 py-2 rounded bg-emerald-200 text-emerald-600 opacity-70">
                         {{ session('password_updated') }}
                     </div>
                     @endif
                     @if (session()->has('password_errors'))
-                    <div class="alert-danger">
+                    <div class="font-semibold w-full px-3 py-2 rounded bg-red-200 text-red-600 opacity-70">
                         {{ session('password_errors') }}
                     </div>
                     @endif
                 </div>
 
                 <div class="text-right mt-2">
-                    <button class="btn-indigo-sm rounded">Update Password</button>
+                    <button
+                    class="rounded px-3 py-2 text-xs font-semibold bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:ring focus:ring-blue-300">
+                        Update Password
+                    </button>
                 </div>
             </form>
         </div>
@@ -172,20 +185,20 @@
                 <div wire:loading wire:target="save">Storing to local...</div>
             </div>
 
-            @error('photo') <div class="invalid-feedback">{{ $message }}</div> @enderror
+            @error('photo') <div class="w-full px-2 py-1 rounded text-pink-600">{{ $message }}</div> @enderror
             <div class="mt-3">
                 @if (session()->has('save_photo_logs'))
-                <div class="alert-success">{{ session('save_photo_logs') }}</div>
+                <div class="font-semibold w-full px-3 py-2 rounded bg-emerald-200 text-emerald-600 opacity-70">{{ session('save_photo_logs') }}</div>
                 @endif
                 @if (session()->has('delete_photo_logs'))
-                <div class="alert-danger">{{ session('delete_photo_logs') }}</div>
+                <div class="font-semibold w-full px-3 py-2 rounded bg-red-200 text-red-600 opacity-70">{{ session('delete_photo_logs') }}</div>
                 @endif
             </div>
             <div class="mt-3">
-                <button class="btn-primary-sm rounded" type="submit">Save Photo</button>
+                <button class="rounded px-3 py-2 text-xs font-semibold bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 focus:ring focus:ring-blue-300" type="submit">Save Photo</button>
             </div>
         </form>
-        <button onclick="confirm('Anda yakin ingin menghapus profile picture ini?') || event.stopImmediatePropagation()" wire:click="deletePhoto" class="btn-danger-sm rounded mt-1">Delete</button>
+        <button onclick="confirm('Anda yakin ingin menghapus profile picture ini?') || event.stopImmediatePropagation()" wire:click="deletePhoto" class="px-2 py-1 font-semibold text-xs bg-pink-500 text-white hover:bg-pink-600 active:bg-pink-700 focus:ring focus:ring-pink-300 rounded mt-1">Delete</button>
     </div>
     <script>
         // Livewire.on('chooseImage', ()=>{
